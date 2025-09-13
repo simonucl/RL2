910fcc3ca35e5d0cbb0d3fe2d001a494228cb99f
simon <simon011130@gmail.com>
2025-09-12 20:40:29 -0700

    update

--- examples/synthetic/synthetic_gsm_offline_rl.sh
+++ examples/synthetic/synthetic_gsm_offline_rl.sh
@@ -90,12 +90,50 @@ NEGATIVE_LABEL_SCALE=0.05
 #     trainer.experiment_name=llama-3.2-1b-combined-inst-offline-rl \
 #     trainer.n_epochs=3
 
+torchrun \
+    --nproc_per_node=4 \
+    -m RL2.trainer.offline_rl \
+    data.path=simonycl/gsm8k_training_negative_multi_turn_1k_${DATASET_MODEL}_transformed \
+    data.max_length=8192 \
+    data.batch_size=64 \
+    actor.model_name=meta-llama/Llama-3.2-1B-Instruct \
+    actor.sp_size=4 \
+    actor.max_length_per_device=4096 \
+    actor.lr=1e-5 \
+    offline_rl.positive_label_scale=1.0 \
+    offline_rl.negative_label_scale=${NEGATIVE_LABEL_SCALE} \
+    trainer.project=synthetic_gsm \
+    trainer.experiment_name=llama-3.2-1b-multi-turn-inst-offline-rl \
+    trainer.n_epochs=3
+
+sleep 30
+
+torchrun \
+    --nproc_per_node=4 \
+    -m RL2.trainer.offline_rl \
+    data.path=simonycl/gsm8k_training_negative_chain_of_thought_1k_${DATASET_MODEL}_transformed \
+    data.max_length=8192 \
+    data.batch_size=64 \
+    actor.model_name=meta-llama/Llama-3.2-1B-Instruct \
+    actor.sp_size=4 \
+    actor.max_length_per_device=4096 \
+    actor.lr=1e-5 \
+    offline_rl.positive_label_scale=1.0 \
+    offline_rl.negative_label_scale=${NEGATIVE_LABEL_SCALE} \
+    trainer.project=synthetic_gsm \
+    trainer.experiment_name=llama-3.2-1b-chain-of-thought-inst-offline-rl \
+    trainer.n_epochs=3
+
+sleep 30
+
 MODELS=(
-    "ckpts/llama-3.2-1b-positive-only"
-    "ckpts/llama-3.2-1b-direct-inst-offline-rl"
-    "ckpts/llama-3.2-1b-sequence-inst-offline-rl"
-    "ckpts/llama-3.2-1b-vs-standard-inst-offline-rl"
-    "ckpts/llama-3.2-1b-combined-inst-offline-rl"
+    # "ckpts/llama-3.2-1b-positive-only"
+    # "ckpts/llama-3.2-1b-direct-inst-offline-rl"
+    # "ckpts/llama-3.2-1b-sequence-inst-offline-rl"
+    # "ckpts/llama-3.2-1b-vs-standard-inst-offline-rl"
+    # "ckpts/llama-3.2-1b-combined-inst-offline-rl"
+    "ckpts/llama-3.2-1b-multi-turn-inst-offline-rl"
+    "ckpts/llama-3.2-1b-chain-of-thought-inst-offline-rl"
 )
 
 for MODEL in "${MODELS[@]}"; do    
