06567d906be1f6399b0631a4bd74245a41aaef2b
simon <simon011130@gmail.com>
2025-10-06 23:07:24 -0700

    add examples

--- examples/gem_reinforce_lora.sh
+++ examples/gem_reinforce_lora.sh
@@ -5,6 +5,12 @@ torchrun \
     test_data.prompts_per_rollout=64 \
     actor.model_name=Qwen/Qwen3-4B-Base \
     actor.max_length_per_device=8192 \
+    actor.lr=1e-4 \
+    actor.tp_size=1 \
+    actor.lora.use_lora=true \
+    actor.lora.r=16 \
+    actor.lora.lora_alpha=32 \
+    actor.lora.lora_dropout=0.05 \
     rollout.train_sampling_params.max_new_tokens=4096 \
     rollout.env_path=envs/gem.py \
     rollout.max_turns=30 \
@@ -12,6 +18,6 @@ torchrun \
     adv.norm_var=true \
     adv.estimator=reinforce \
     trainer.project=GEM \
-    trainer.experiment_name=multi-env_qwen3-4b_reinforce \
-    trainer.n_epochs=512 \
+    trainer.experiment_name=multi-env_qwen3-4b_reinforce_lora \
+    trainer.n_epochs=200 \
     trainer.save_freq=64
--- examples/gem_reinforce_single.sh
+++ examples/gem_reinforce_single.sh
@@ -6,12 +6,12 @@ torchrun \
     actor.model_name=Qwen/Qwen3-4B-Base \
     actor.max_length_per_device=8192 \
     rollout.train_sampling_params.max_new_tokens=4096 \
-    rollout.env_path=envs/gem.py \
+    rollout.env_path=envs/sudoku.py \
     rollout.max_turns=30 \
     adv.global_norm=true \
     adv.norm_var=true \
     adv.estimator=reinforce \
     trainer.project=GEM \
-    trainer.experiment_name=multi-env_qwen3-4b_reinforce \
-    trainer.n_epochs=512 \
+    trainer.experiment_name=sudoku_qwen3-4b_reinforce \
+    trainer.n_epochs=200 \
     trainer.save_freq=64
