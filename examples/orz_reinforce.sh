c1dd60f22ca6484ba3538094629ba9e263d9c3c7
simon <simon011130@gmail.com>
2025-10-08 13:47:00 -0400

    add olmoe exps

--- examples/orz_reinforce.sh
+++ examples/orz_reinforce.sh
@@ -1,11 +1,13 @@
+export no_proxy=127.0.0.1:7890,localhost,127.0.0.1,10.99.103.147,10.99.103.*
+
 torchrun \
-    --nproc_per_node=4 \
+    --nproc_per_node=8 \
     -m RL2.trainer.ppo \
     train_data.path=Chenmien/OpenReasonerZero \
     train_data.prompts_per_rollout=128 \
     train_data.responses_per_prompt=64 \
     test_data.path=Chenmien/OlympiadBench \
-    actor.model_name=Qwen/Qwen2.5-7B \
+    actor.model_name=Qwen/Qwen3-1.7B-Base \
     actor.sp_size=2 \
     actor.max_length_per_device=8192 \
     rollout.train_sampling_params.max_new_tokens=8192 \
