106e67b1b303dd0a7dbc7699f35acdbfd5474516
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-10-09 20:13:03 +0800

    fix logic of routing backend

--- RL2/workers/__init__.py
+++ RL2/workers/__init__.py
@@ -1,21 +1,28 @@
+from hydra.core.hydra_config import HydraConfig
 from .fsdp import FSDPActor, FSDPCritic
 from .megatron import MegatronActor, MegatronCritic
 from .rollout import Rollout
 
+hydra_config = HydraConfig.get()
+
 def initialize_actor(config, train):
 
-    if config.backend == "fsdp":
+    backend = hydra_config.runtime.choices.get(
+        "actor" if train else "ref_actor"
+    )
+    if backend == "fsdp":
         return FSDPActor(config, train)
-    elif config.backend == "megatron":
+    elif backend == "megatron":
         return MegatronActor(config, train)
     else:
         raise NotImplementedError
 
 def initialize_critic(config):
 
-    if config.backend == "fsdp":
+    backend = hydra_config.runtime.choices.get("critic")
+    if backend == "fsdp":
         return FSDPCritic(config)
-    elif config.backend == "megatron":
+    elif backend == "megatron":
         return MegatronCritic(config)
     else:
         raise NotImplementedError
