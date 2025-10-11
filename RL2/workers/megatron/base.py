d78eb485d59456b24fffe892bdd0ba811b3b0f3f
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-10-11 12:49:20 +0800

    add prepare_scheduler for MegatronWorker

--- RL2/workers/megatron/base.py
+++ RL2/workers/megatron/base.py
@@ -6,6 +6,7 @@ from megatron.core import (
     tensor_parallel
 )
 from megatron.core.optimizer import OptimizerConfig, get_megatron_optimizer
+from megatron.core.optimizer_param_scheduler import OptimizerParamScheduler
 from mbridge import AutoBridge
 from RL2.workers import Worker
 from RL2.utils.megatron.context_parallelism import slide_along_cp
@@ -47,13 +48,6 @@ class MegatronWorker(Worker):
             )
             tensor_parallel.model_parallel_cuda_manual_seed(42)
 
-        self.device_mesh = {
-            "pp": mpu.get_pipeline_model_parallel_group(),
-            "dp": mpu.get_data_parallel_group(),
-            "cp": mpu.get_context_parallel_group(),
-            "tp": mpu.get_tensor_model_parallel_group()
-        }
-
     def prepare_model_optimizer(self):
         
         self.bridge.load_weights(self.model, self.config.model_name)
@@ -68,3 +62,25 @@ class MegatronWorker(Worker):
             self.optimizer = get_megatron_optimizer(
                 optimizer_config, self.model
             )
+
+    def prepare_scheduler(self, total_steps):
+
+        num_training_steps = total_steps * getattr(
+            self.config, "update_per_rollout", 1
+        )
+        lr_warmup_steps = int(self.config.warmup_ratio * num_training_steps)
+        lr_decay_steps = num_training_steps - lr_warmup_steps
+
+        return OptimizerParamScheduler(
+            self.optimizer,
+            init_lr=0.0,
+            max_lr=self.config.optimizer.lr,
+            min_lr=0.0,
+            lr_warmup_steps=lr_warmup_steps,
+            lr_decay_steps=lr_decay_steps,
+            lr_decay_style=self.config.scheduler,
+            start_wd=self.config.optimizer.weight_decay,
+            end_wd=self.config.optimizer.weight_decay,
+            wd_incr_steps=0,
+            wd_incr_style="constant"
+        )
