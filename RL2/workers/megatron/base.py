fix bug for broadcast along pp

RL2/workers/megatron/base.py
@@ -210,7 +210,7 @@ class MegatronWorker(Worker):
         output = broadcast_object(
             output,
             group=mpu.get_pipeline_model_parallel_group(),
-            group_src=mpu.get_pipeline_model_parallel_last_rank()
+            group_src=mpu.get_pipeline_model_parallel_world_size() - 1
         )
         if torch.is_grad_enabled():
             self.load_optimizer_to_device(torch.cuda.current_device())