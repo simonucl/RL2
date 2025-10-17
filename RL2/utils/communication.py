385231d8a84b2060bd4420dae38163fe7ebfeded
simonucl <simon011130@gmail.com>
2025-10-17 17:22:01 -0400

    fix bugs for megatron process group

--- RL2/utils/communication.py
+++ RL2/utils/communication.py
@@ -9,19 +9,26 @@ def initialize_global_process_group(timeout_second=36000):
     local_rank = int(os.environ["LOCAL_RANK"])
     torch.cuda.set_device(local_rank)
 
+def _unwrap_process_group(process_group):
+    if hasattr(process_group, '_wrapped'):
+        process_group = process_group._wrapped
+    elif hasattr(process_group, 'group'):
+        process_group = process_group.group
+    return process_group
+
 def broadcast_object(obj, src=None, group=None, group_src=None):
-    
     object_list = [obj]
     dist.broadcast_object_list(
         object_list,
         src=src,
-        group=group,
+        group=_unwrap_process_group(group),
         group_src=group_src
     )
     return object_list[0]
 
 def gather_and_concat_list(lst, process_group):
 
+    process_group = _unwrap_process_group(process_group)
     lists = (
         dist.get_world_size(process_group) * [None]
         if dist.get_rank(process_group) == 0
