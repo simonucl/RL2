9636174cfeda0224fc0fa7b39716ceb04be33452
simon <simon011130@gmail.com>
2025-10-17 19:07:00 +0000

    add qwen moe

---
+++ examples/qwen_moe.sh
@@ -0,0 +1,26 @@
+torchrun \
+    --nproc_per_node=8 \
+    -m RL2.trainer.ppo \
+    actor=megatron \
+    ref_actor=megatron \
+    critic=megatron \
+    train_data.path=Chenmien/OpenReasonerZero \
+    train_data.prompts_per_rollout=128 \
+    train_data.responses_per_prompt=64 \
+    test_data.path=Chenmien/OlympiadBench \
+    actor.model_name=Qwen/Qwen3-30B-A3B-Base \
+    actor.pp_size=1 \
+    actor.cp_size=1 \
+    actor.tp_size=2 \
+    actor.ep_size=2 \
+    actor.etp_size=2 \
+    actor.max_length_per_device=8192 \
+    actor.optimizer.lr=5e-7 \
+    rollout.server_args.tp_size=2 \
+    rollout.train_sampling_params.max_new_tokens=8192 \
+    rollout.env_path=envs/orz.py \
+    adv.estimator=reinforce \
+    trainer.project=QwenMoE \
+    trainer.experiment_name=qwen3-30b-a3b \
+    trainer.test_freq=8 \
+    trainer.save_freq=32
