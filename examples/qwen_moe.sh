397528cf9088e405d36d253aff86e1ec095c2951
simon <simon011130@gmail.com>
2025-10-17 21:22:32 +0000

    fix megatron distributed errors

--- examples/qwen_moe.sh
+++ examples/qwen_moe.sh
@@ -1,3 +1,6 @@
+# hf download qwen/Qwen3-4B-Base --local-dir /root/Qwen3-4B-Base
+# hf download qwen/Qwen3-30B-A3B-Base --local-dir /root/Qwen3-30B-A3B-Base
+
 torchrun \
     --nproc_per_node=8 \
     -m RL2.trainer.ppo \
@@ -5,22 +8,22 @@ torchrun \
     ref_actor=megatron \
     critic=megatron \
     train_data.path=Chenmien/OpenReasonerZero \
-    train_data.prompts_per_rollout=128 \
-    train_data.responses_per_prompt=64 \
+    train_data.prompts_per_rollout=16 \
+    train_data.responses_per_prompt=4 \
     test_data.path=Chenmien/OlympiadBench \
-    actor.model_name=Qwen/Qwen3-30B-A3B-Base \
+    actor.model_name=/root/Qwen3-30B-A3B-Base \
     actor.pp_size=1 \
     actor.cp_size=1 \
-    actor.tp_size=2 \
     actor.ep_size=2 \
-    actor.etp_size=2 \
+    actor.tp_size=4 \
     actor.max_length_per_device=8192 \
     actor.optimizer.lr=5e-7 \
     rollout.server_args.tp_size=2 \
-    rollout.train_sampling_params.max_new_tokens=8192 \
+    rollout.server_args.mem_fraction_static=0.85 \
+    rollout.train_sampling_params.max_new_tokens=5000 \
     rollout.env_path=envs/orz.py \
     adv.estimator=reinforce \
     trainer.project=QwenMoE \
-    trainer.experiment_name=qwen3-30b-a3b \
+    trainer.experiment_name=qwen3-30b-a3b-base \
     trainer.test_freq=8 \
     trainer.save_freq=32
